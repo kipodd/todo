f0061ecff39973adef37da1ad1bebdff9aff8549
kipodd <17299316+kipodd@users.noreply.github.com>
2020-10-17 02:33:09 +0300

    Changed some lists to tuples

--- main.py
+++ main.py
@@ -7,32 +7,32 @@ done = ["Finish todo app"]
 
 sg.theme("Reddit")
 
-layout = [
+layout = (
     [sg.Text("To Do:")],
     [sg.Listbox(values=todos, size=(64, 7), key="todos")],
     [sg.Text("Done:")],
     [sg.Listbox(values=done, size=(64, 7), key="done")],
-    [
+    (
         sg.Button(" Add ", key="add"),
         sg.Button(" Edit ", key="edit"),
         sg.Button(" Delete ", key="delete"),
         sg.Button(" Mark as Done ", key="mark_done"),
         sg.Button(" Check everything, I need a beer! ", key="mark_all_done")
-    ]
-]
+    )
+)
 
 window = sg.Window("To Do", layout)
 while True:
     event, values = window.Read()
 
-    if event == "add" and (new_task := sg.popup_get_text("Add task:", no_titlebar=True)) \
-            and new_task not in set(todos):
-        todos.append(new_task)
+    if event == "add" and (new_todo := sg.popup_get_text("Add task:", no_titlebar=True)) \
+            and new_todo not in set(todos):
+        todos.append(new_todo)
         window["todos"].Update(values=todos)
     elif event == "edit" and (selected := values["todos"] and values["todos"][0]) \
-            and (new_task := sg.popup_get_text("Edit task:", no_titlebar=True)) \
-            and new_task not in set(todos):
-        todos = [t if t != selected else new_task for t in todos]
+            and (new_todo := sg.popup_get_text("Edit task:", no_titlebar=True)) \
+            and new_todo not in set(todos):
+        todos = [t if t != selected else new_todo for t in todos]
         window["todos"].Update(values=todos)
     elif event == "delete" and (selected := values["todos"] and values["todos"][0]):
         todos.remove(selected)
